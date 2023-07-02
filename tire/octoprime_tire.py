ccfd9ce6a09b686a0cd05efc2ddff3a373fe3f5e
smetana228 <65881878+smetana228@users.noreply.github.com>
2023-07-02 20:57:02 +0600

    Add files via upload

---
+++ tire/octoprime_tire.py
@@ -0,0 +1,12 @@
+from tire.tire import Tire
+
+class OctoprimeTire(Tire):
+
+	def __init__(self,tires):
+		self.tires=tires
+
+	def needs_service(self):
+		tires_worn=0
+		for x in tires:
+			tires_worn=+x
+		return tires_worn >= 3
