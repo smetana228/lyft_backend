99c4620c369f47d1861f789c0703e1c05983a4ef
smetana228 <65881878+smetana228@users.noreply.github.com>
2023-07-02 21:03:04 +0600

    Update car_factory.py

--- car_factory.py
+++ car_factory.py
@@ -3,6 +3,8 @@ from battery.spindler_battery import SpindlerBattery
 from engine.sternman_engine import SternmanEngine
 from engine.capulet_engine import CapuletEngine
 from engine.willoughby_engine import WilloughByEngine
+from tire.carrigan_tire import CarriganTire
+from tire.octoprime_tire import OctoprimeTire
 from car import Car
 
 class CarFactory:
@@ -10,7 +12,8 @@ class CarFactory:
 	def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
 		engine = CapuletEngine(last_service_mileage, current_mileage)
 		battery = SpindlerBattery(current_date, last_service_date)
-		car = Car(engine,battery)
+		tire = CarriganTire(tires)
+		car = Car(engine,battery,tire)
 
 		return car
 
@@ -18,7 +21,8 @@ class CarFactory:
 	def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
 		engine = WilloughByEngine(current_mileage, last_service_mileage)
 		battery = SpindlerBattery(current_date, last_service_date)
-		car = Car(battery,engine)
+		tire = OctoprimeTire(tires)
+		car = Car(battery,engine,tire)
 
 		return car
 
@@ -26,7 +30,8 @@ class CarFactory:
 	def create_palindrome(current_date, last_service_date, warning_light_on):
 		engine = SternmanEngine(warning_light_on)
 		battery = SpindlerBattery(current_date, last_service_date)
-		car = Car(battery,engine)
+		tire = CarriganTire(tires)
+		car = Car(battery,engine,tire)
 
 		return car 
 
@@ -34,7 +39,8 @@ class CarFactory:
 	def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
 		engine = WilloughByEngine(current_mileage, last_service_mileage)
 		battery = NubbinBattery(current_date, last_service_date)
-		car = Car(battery,engine)
+		tire = OctoprimeTire(tires)
+		car = Car(battery,engine, tire)
 
 		return car 
 
@@ -42,6 +48,7 @@ class CarFactory:
 	def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
 		engine = CapuletEngine(last_service_mileage, current_mileage)
 		battery = NubbinBattery(current_date, last_service_date)
-		car = Car(battery,engine)
+		tire = CarriganTire(tires)
+		car = Car(battery,engine, tire)
 
 		return car  
